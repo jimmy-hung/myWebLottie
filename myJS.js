b6c18709de24ae151d51e2c9cfd01718cc459c38
洪立德 <jimmy@honglidede-MacBook-Pro.local>
2019-01-05 12:52:35 +0800

    test

--- myJS.js
+++ myJS.js
@@ -1,10 +1,26 @@
-/* Shapes */
-var svgContainer = document.getElementById('svgContainer');
-var animItem = bodymovin.loadAnimation({
-  wrapper: svgContainer,
-  animType: 'svg',
-  loop: true,
-  path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
-});
+//lettering js
+$(".loading").lettering();
+
+// var animation = bodymovin.loadAnimation({
+//   container: document.getElementById('bm'),
+//   renderer: 'svg',
+//   loop: true,
+//   autoplay: true,
+//   path: ''
+// })
+
+
+window.onload=function(){
+  $.ajax({ 
+    type: 'GET', 
+    url:'http://52.175.12.176/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23',
+    dataType: 'json',
+    //crossDomain: true,
+    error:function(xhr){
+    alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
+    }
+  });
+}
+
 
 alert("Hello");
