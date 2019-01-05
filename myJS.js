15f3b5073047e362958a93a70b051ed4a896d3aa
洪立德 <jimmy@honglidede-MacBook-Pro.local>
2019-01-05 20:12:13 +0800

    test again2

--- myJS.js
+++ myJS.js
@@ -1,46 +1,51 @@
 //lettering js
 // $(".loading").lettering();
 
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'default.json'
-})
 
 
-// window.onload=function(){
-//   $.ajax({ 
-//     type: 'GET', 
-//     url:'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',
-//     dataType: 'json',
-    // crossDomain: true,
+
+window.onload=function(){
+  $.ajax({ 
+    type: 'GET', 
+    url:'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',
+    dataType: 'json',
+    crossDomain: true,
     
-    // success: function (response){
+    success: function (response){
 
-    //   var isOpen = response.version
-    //   var newUrl = response.msg.new_url
 
-    //   console.log(response);
-    //   console.log(response.msg.is_active);
-    //   console.log(isOpen)
-    //   console.log(newUrl)
+      var isOpen = response.version
+      var newUrl = response.msg.new_url
+      var commentUulJson = response.msg.comment
 
-    //   if (isOpen = 2){
-    //     window.location = newUrl
+      console.log(response);
+      console.log(response.msg.is_active);
+      console.log(isOpen)
+      console.log(newUrl)
+
+      if (isOpen = 2){
+        // window.location = newUrl
+        var animation = bodymovin.loadAnimation({
+
+            container: document.getElementById('bm'),
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: commentUulJson,
+           
+           })
         
-    //   }
-    // },
+      }
+    },
 
 
     
 
-//     error:function(xhr){
-//     alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
-//     }
-//   });
-// }
+    error:function(xhr){
+    alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
+    }
+  });
+}
 
 
-// alert("Hello");
+alert("Hello");
