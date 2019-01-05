1398c38205bc4f55e57af9401c6706f69b5d47c2
洪立德 <jimmy@honglidede-MacBook-Pro.local>
2019-01-05 19:25:47 +0800

    test again

--- myJS.js
+++ myJS.js
@@ -1,46 +1,46 @@
 //lettering js
-$(".loading").lettering();
+// $(".loading").lettering();
 
-// var animation = bodymovin.loadAnimation({
-//   container: document.getElementById('bm'),
-//   renderer: 'svg',
-//   loop: true,
-//   autoplay: true,
-//   path: ''
-// })
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'default.json'
+})
 
 
-window.onload=function(){
-  $.ajax({ 
-    type: 'GET', 
-    url:'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',
-    dataType: 'json',
+// window.onload=function(){
+//   $.ajax({ 
+//     type: 'GET', 
+//     url:'https://app-versions.herokuapp.com/index/index/4c59282b-eb9b-4b74-86c7-0fd2d338667b',
+//     dataType: 'json',
     // crossDomain: true,
     
-    success: function (response){
+    // success: function (response){
 
-      var isOpen = response.version
-      var newUrl = response.msg.new_url
+    //   var isOpen = response.version
+    //   var newUrl = response.msg.new_url
 
-      console.log(response);
-      console.log(response.msg.is_active);
-      console.log(isOpen)
-      console.log(newUrl)
+    //   console.log(response);
+    //   console.log(response.msg.is_active);
+    //   console.log(isOpen)
+    //   console.log(newUrl)
 
-      if (isOpen = 2){
-        window.location = newUrl
+    //   if (isOpen = 2){
+    //     window.location = newUrl
         
-      }
-    },
+    //   }
+    // },
 
 
     
 
-    error:function(xhr){
-    alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
-    }
-  });
-}
+//     error:function(xhr){
+//     alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
+//     }
+//   });
+// }
 
 
-alert("Hello");
+// alert("Hello");
